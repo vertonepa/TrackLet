fbb218bfbfce483e1dabd2fa54e5b6f1318c77a5
Pablo Vertone <113537687+vertonepa@users.noreply.github.com>
2025-08-13 21:32:09 -0300

    uiState cambia forma de observar datos

--- app/src/main/java/com/vertonepa/tracklet/tickets/presentation/ticket_list/TicketListViewModel.kt
+++ app/src/main/java/com/vertonepa/tracklet/tickets/presentation/ticket_list/TicketListViewModel.kt
@@ -7,32 +7,33 @@ import com.vertonepa.tracklet.tickets.domain.usecases.DeleteTicketByIdUseCase
 import com.vertonepa.tracklet.tickets.domain.usecases.GetTicketsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TicketListViewModel @Inject constructor(
-    private val getTicketsUseCase: GetTicketsUseCase,
+    getTicketsUseCase: GetTicketsUseCase,
     private val deleteTicketByIdUseCase: DeleteTicketByIdUseCase,
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<TicketListUIState>(TicketListUIState.Loading)
-    val uiState = _uiState.asStateFlow()
-
-    init {
-        loadTickets()
-    }
-
-    fun loadTickets() {
-        viewModelScope.launch {
-            getTicketsUseCase().collectLatest { tickets ->
-                _uiState.value = tickets.takeIf { it.isNotEmpty() }
-                    ?.let { TicketListUIState.Success(it) }
-                    ?: TicketListUIState.EmptyList
+    private val _uiState =
+        getTicketsUseCase()
+            .map { tickets ->
+                if (tickets.isEmpty()) TicketListUIState.EmptyList
+                else TicketListUIState.Success(tickets)
             }
-        }
-    }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = TicketListUIState.Loading
+            )
+
+    val uiState: StateFlow<TicketListUIState> = _uiState
 
     fun onDeleteTicket(id: String) {
         viewModelScope.launch {
