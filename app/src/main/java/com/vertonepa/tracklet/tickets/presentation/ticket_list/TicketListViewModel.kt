43d3974bdc27ddd9b7b938d388aeb8a13ffbd2ee
Pablo Vertone <113537687+vertonepa@users.noreply.github.com>
2025-12-07 10:42:55 -0300

    se elimina variable stateflow redundante en viewModel

--- app/src/main/java/com/vertonepa/tracklet/tickets/presentation/ticket_list/TicketListViewModel.kt
+++ app/src/main/java/com/vertonepa/tracklet/tickets/presentation/ticket_list/TicketListViewModel.kt
@@ -6,11 +6,7 @@ import com.vertonepa.tracklet.tickets.domain.model.TicketListModel
 import com.vertonepa.tracklet.tickets.domain.usecases.DeleteTicketByIdUseCase
 import com.vertonepa.tracklet.tickets.domain.usecases.GetTicketsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -21,7 +17,7 @@ class TicketListViewModel @Inject constructor(
     getTicketsUseCase: GetTicketsUseCase,
     private val deleteTicketByIdUseCase: DeleteTicketByIdUseCase,
 ) : ViewModel() {
-    private val _uiState =
+    val uiState =
         getTicketsUseCase()
             .map { tickets ->
                 if (tickets.isEmpty()) TicketListUIState.EmptyList
@@ -32,8 +28,6 @@ class TicketListViewModel @Inject constructor(
                 started = SharingStarted.WhileSubscribed(5000),
                 initialValue = TicketListUIState.Loading
             )
-
-    val uiState: StateFlow<TicketListUIState> = _uiState
 
     fun onDeleteTicket(id: String) {
         viewModelScope.launch {
