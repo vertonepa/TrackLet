f673c8ca19c1cedff557aee93f680c270bd13155
Pablo Vertone <113537687+vertonepa@users.noreply.github.com>
2026-02-19 22:31:03 -0300

    Ahora el proyecto funciona con la jdk 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
         isCoreLibraryDesugaringEnabled = true
     }
     buildFeatures {
         compose = true
+    }
+    kotlin {
+        jvmToolchain(21)
     }
 }
 
